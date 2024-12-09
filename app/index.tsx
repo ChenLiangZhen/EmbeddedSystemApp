6a09b918c5b16c6e7d6f9aee1d3502bd9ddc8a82
lightiichen <lightiichen@gmail.com>
2024-12-09 20:33:21 +0800

    working

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,48 @@
-import { Text, View } from "react-native";
+import { useEvent } from 'expo';
+import { useVideoPlayer, VideoView } from 'expo-video';
+import { StyleSheet, View, Button } from 'react-native';
+import {WIDTH} from "@/helper/dimension";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+const videoSource =
+   'http://192.168.0.109:3000/video/stream.m3u8';
+ 
+export default function VideoScreen() {
+   const player = useVideoPlayer(videoSource, player => {
+      player.loop = true;
+      player.play();
+   });
+   
+   const { isPlaying } = useEvent(player, 'playingChange', { isPlaying: player.playing });
+   
+   return (
+      <View style={{
+         flex: 1,
+         padding: 8,
+         backgroundColor: "white",
+         alignItems: 'center',
+         justifyContent: 'center',
+         paddingHorizontal: 50,
+      }}>
+         <VideoView style={{
+            backgroundColor: "black",
+            borderRadius: 16,
+            width: WIDTH * 0.95,
+            height: 275,
+         }} player={player} allowsFullscreen allowsPictureInPicture />
+         <View style={{
+            padding: 8,
+         }}>
+            <Button
+               title={isPlaying ? 'Pause' : 'Play'}
+               onPress={() => {
+                  if (isPlaying) {
+                     player.pause();
+                  } else {
+                     player.play();
+                  }
+               }}
+            />
+         </View>
+      </View>
+   );
 }
